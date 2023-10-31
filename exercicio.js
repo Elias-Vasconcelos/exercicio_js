f8b0a247e9a05a7b207e0af9bfc43cf352c13908
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2023-10-31 19:38:58 -0300

    subundo a branch

--- exercicio.js
+++ exercicio.js
@@ -16,5 +16,7 @@
             }else {
                 document.querySelector('.mensagem').style.display = 'none';
                 document.querySelector('.mensagemSucesso').style.display = 'block'
+                campoB.value = ''
+                campoA.value = ''
             }
         })
