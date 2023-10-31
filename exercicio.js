c8d66320c1c75e36fc9a5f09e8af89a4edf3fd22
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2023-10-31 19:33:48 -0300

    formulaçao da atividade

---
+++ exercicio.js
@@ -0,0 +1,20 @@
+        const form = document.querySelector('.FormularioDeValidaçao')
+        const campoA  = document.getElementById('CampoA')
+        const campoB  = document.getElementById('CampoB')
+        let Validaform = false
+
+    function validaCamposa (campoA, campoB){
+        let valoresDosCampos = campoA < campoB;
+        return valoresDosCampos;
+    }
+    
+
+    form.addEventListener('submit', function (e){
+        e.preventDefault();
+            if(!Validaform){
+                document.querySelector('.mensagem').style.display = 'block';
+            }else {
+                document.querySelector('.mensagem').style.display = 'none';
+                document.querySelector('.mensagemSucesso').style.display = 'block'
+            }
+        })
