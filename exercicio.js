0d843927b59f6160b096a0a770402fdff37a1d1a
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2023-11-03 20:44:57 -0300

    ajustes

--- exercicio.js
+++ exercicio.js
@@ -1,29 +1,30 @@
         const form = document.querySelector('.FormularioDeValidaçao')
         let campoA  = document.getElementById('CampoA');
         let campoB  = document.getElementById('CampoB');
-        const MensageSucesso = document.getElementsByClassName('mensagemSucesso')
-        const MensagemErro = document.getElementsByClassName('mensagem')
 
 
-    function validaCamposa (campoA, campoB){
-        let valoresDosCampos = campoA < campoB;
-        return valoresDosCampos;
-    }
+        function validaCampos (campoA, campoB){
+            let valoresDosCampos = campoA < campoB;
+            return valoresDosCampos;
+        } 
     
 
     form.addEventListener('submit', function (e){
         e.preventDefault();
+        
+
         campoB = parseInt(campoB.value);
         campoA = parseInt(campoA.value);
 
-            if(!validaCamposa(campoA, campoB)) {
+            if(!validaCampos(campoA, campoB)){
                 document.querySelector('.mensagem').style.display = 'block';
                 document.querySelector('.mensagemSucesso').style.display = 'none'
-            }else{
+            }else {
                 document.querySelector('.mensagem').style.display = 'none';
                 document.querySelector('.mensagemSucesso').style.display = 'block';
-                campoB.value = '';
-                campoA.value = '';
+                campoA.value = ''
+                campoB.value = ''
             }
+
 
         })
